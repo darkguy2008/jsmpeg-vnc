bd90f41d6e0a707840d29a92fd5461cca3756052
Ben <daprogramerforbatch@gmail.com>
2018-07-21 11:30:44 -0700

    Middle click support
    Before middle click would act as a left mouse butten, but the programing has been updated to support middle click.
    Tested in Chrome, Firefox, IE11, and Edge.

--- client/jsmpg-vnc.js
+++ client/jsmpg-vnc.js
@@ -32,8 +32,11 @@ var KEY_DOWN = 0x01,
 	MOUSE_1_DOWN = 0x0002,
 	MOUSE_1_UP = 0x0004,
 	MOUSE_2_DOWN = 0x0008,
-	MOUSE_2_UP = 0x0010;
-	MOUSEEVENTF_WHEEL = 0x0800;
+	MOUSE_2_UP = 0x0010,
+	MOUSEEVENTF_WHEEL = 0x0800,
+	MOUSEEVENTF_MIDDLEUP = 0x0040,
+	MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+
 // struct input_key_t { uint16 type, uint16 state; uint16 key_code; }
 var sendKey = function(ev, action, key) {
 	client.send(new Uint16Array([INPUT_KEY, action, key]));
@@ -65,7 +68,7 @@ var sendMouse = function(ev, action) {
 		
 		var p = ev.changedTouches ? ev.changedTouches[0] : ev;
 		
-		// FUCK, DID I MENTION I LOOOOOVE VENDOR PREFIXES? SO USEFUL!
+		// FUCK, DID I MENTION I LOOOOOVE VENDOR PREFIXES? SO USEFUL! 
 		x = p.movementX || p.mozMovementX || p.webkitMovementX;
 		y = p.movementY || p.mozMovementY || p.webkitMovementY;
 
@@ -108,12 +111,59 @@ window.addEventListener('keydown', function(ev) { sendKey(ev, KEY_DOWN, ev.keyCo
 window.addEventListener('keyup', function(ev) { sendKey(ev, KEY_UP, ev.keyCode); }, false );
 
 // Mouse
+
 canvas.addEventListener('mousemove', function(ev){ sendMouse(ev, null); }, false);
-canvas.addEventListener('mousedown', function(ev){ sendMouse(ev, ev.button == 2 ? MOUSE_2_DOWN : MOUSE_1_DOWN); }, false);
-canvas.addEventListener('mouseup', function(ev){ sendMouse(ev, ev.button == 2 ? MOUSE_2_UP : MOUSE_1_UP); }, false);
-canvas.addEventListener('onmousewheel', function(ev){ sendMouse(ev, MOUSEEVENTF_WHEEL); }, false);
-canvas.addEventListener('mousewheel', function(ev){ sendMouse(ev, MOUSEEVENTF_WHEEL); }, false);
-canvas.addEventListener('DOMMouseScroll', function(ev){ sendMouse(ev, MOUSEEVENTF_WHEEL); }, false);
+function btndwn(ev){
+	var ev = ev || window.event;
+    var btnCode;
+
+        btnCode = ev.button;
+
+        switch (btnCode) {
+            case 0:
+                sendMouse(ev, MOUSE_1_DOWN);
+            break;
+
+            case 1:
+                sendMouse(ev, MOUSEEVENTF_MIDDLEDOWN);
+            break;
+
+            case 2:
+                sendMouse(ev, MOUSE_2_DOWN);
+            break;
+
+            default:
+                console.log('Unexpected code: ' + btnCode);
+        }
+}
+function btnup(ev){
+	var ev = ev || window.event;
+    var btnCode;
+
+        btnCode = ev.button;
+
+        switch (btnCode) {
+            case 0:
+                sendMouse(ev, MOUSE_1_UP);
+            break;
+
+            case 1:
+                sendMouse(ev, MOUSEEVENTF_MIDDLEUP);
+            break;
+
+            case 2:
+                sendMouse(ev, MOUSE_2_UP);
+            break;
+
+            default:
+                console.log('Unexpected code: ' + btnCode);
+        }
+}
+canvas.addEventListener('mousedown', function(ev){ btndwn(ev); });
+canvas.addEventListener('mouseup', function(ev){ btnup(ev); });
+canvas.addEventListener('onmousewheel', function(ev){ sendMouse(ev, MOUSEEVENTF_WHEEL); });
+canvas.addEventListener('mousewheel', function(ev){ sendMouse(ev, MOUSEEVENTF_WHEEL); });
+canvas.addEventListener('DOMMouseScroll', function(ev){ sendMouse(ev, MOUSEEVENTF_WHEEL); });
 
 // Touch
 canvas.addEventListener('touchstart', function(ev){
