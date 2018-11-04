3e7850c9878251dac9195c7a80c46c51082b96ef
Ben Nottelling <daprogramerforbatch@gmail.com>
2018-11-04 10:01:03 -0800

    backport color fixes
    Yay, this looks much nicer :D

--- client/jsmpg.js
+++ client/jsmpg.js
@@ -2530,25 +2530,27 @@ var
 	START_USER_DATA = 0xB2,
 
 	// Shaders for accelerated WebGL YCbCrToRGBA conversion
-	SHADER_FRAGMENT_YCBCRTORGBA = [
+		SHADER_FRAGMENT_YCBCRTORGBA= [
 		'precision mediump float;',
 		'uniform sampler2D YTexture;',
 		'uniform sampler2D CBTexture;',
 		'uniform sampler2D CRTexture;',
 		'varying vec2 texCoord;',
 
+		'mat4 rec601 = mat4(',
+			'1.16438,  0.00000,  1.59603, -0.87079,',
+			'1.16438, -0.39176, -0.81297,  0.52959,',
+			'1.16438,  2.01723,  0.00000, -1.08139,',
+			'0, 0, 0, 1',
+		');',
+
 		'void main() {',
 			'float y = texture2D(YTexture, texCoord).r;',
-			'float cr = texture2D(CBTexture, texCoord).r - 0.5;',
-			'float cb = texture2D(CRTexture, texCoord).r - 0.5;',
+			'float cb = texture2D(CBTexture, texCoord).r;',
+			'float cr = texture2D(CRTexture, texCoord).r;',
 
-			'gl_FragColor = vec4(',
-				'y + 1.4 * cr,',
-				'y + -0.343 * cb - 0.711 * cr,',
-				'y + 1.765 * cb,',
-				'1.0',
-			');',
-		'}'
+			'gl_FragColor = vec4(y, cr, cb, 1.0) * rec601;',
+			'}',
 	].join('\n'),
 
 	SHADER_FRAGMENT_LOADING = [
