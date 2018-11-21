a97f17ec2a265064809601d72a6e5e28d9e909c8
Ben <daprogramerforbatch@gmail.com>
2018-11-21 09:58:10 -0800

    Mobile client updates (#12)
    * Started to implement joydiv
    
    This will creat much nicer mobile controlls when done!
    
    * Inital working version.
    
    Up next is a mouse controller on the right side of the screen.
    
    * Reposition joystick, scale on small screens
    
    and a minor change to the controller visuals. I think it looks much nicer now
    
    * Not inclduded in last commit??
    
    * Moved joystick JS to seperate file
    
    This is just cleaner imo

---
+++ client/joydiv/joys.js
@@ -0,0 +1,47 @@
+    var element = document.getElementById('controller');
+    var joydiv = new JoydivModule.Joydiv({'element':element});
+    element.addEventListener('joydiv-changed',function(e){
+	  switch(joydiv.getOneOf8Directions().name) {
+		case "up":
+		sendKey(null, KEY_DOWN, 87);//W
+		break;
+		case "down":
+		sendKey(null, KEY_DOWN, 83);//S
+		break;
+		
+		case "left":
+		sendKey(null, KEY_DOWN, 65);//A
+		break;
+		
+		case "right":
+		sendKey(null, KEY_DOWN, 68);//D
+		break;
+		
+		case "up-left":
+		sendKey(null, KEY_DOWN, 87);//W
+		sendKey(null, KEY_DOWN, 65);//A
+		break;
+		
+		case "up-right":
+		sendKey(null, KEY_DOWN, 87);//W
+		sendKey(null, KEY_DOWN, 68);//D
+		break;
+		
+		case "down-left":
+		sendKey(null, KEY_DOWN, 83);//S
+		sendKey(null, KEY_DOWN, 65);//A
+		break;
+		
+		case "down-right":
+		sendKey(null, KEY_DOWN, 83);//S
+		sendKey(null, KEY_DOWN, 68);//D
+		break;
+		
+		case "none":
+		sendKey(null, KEY_UP, 87);//W
+		sendKey(null, KEY_UP, 83);//S
+		sendKey(null, KEY_UP, 65);//A
+		sendKey(null, KEY_UP, 68);//D
+		break;
+	  }
+    });
